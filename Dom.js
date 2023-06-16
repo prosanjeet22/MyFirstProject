f9678f972317e05ac11b6337b5728a481cc452ee
prosanjeet <73186809+prosanjeet22@users.noreply.github.com>
2023-06-16 22:52:16 +0530

    Added the item add and remove functionality

--- Dom.js
+++ Dom.js
@@ -14,7 +14,7 @@
 //     items[i].style.fontStyle="italic"
 // }
 
-let itemList=document.querySelector('#items');
+//let itemList=document.querySelector('#items');
 // console.log(itemList.parentNode);
 // console.log(itemList.parentNode.parentNode);
 // console.log(itemList.parentNode.parentNode.parentNode);
@@ -33,12 +33,51 @@ let itemList=document.querySelector('#items');
 // console.log(itemList.nextSibling);
 // console.log(itemList.nextElementSibling)
 // console.log(itemList.previousElementSibling)
-itemList.previousElementSibling.style.backgroundColor="yellow";
- let div=document.createElement('div');
- div.className="parentDiv";
- div.id="newDiv";
- div.setAttribute('title',"helloDiv");
- let textNode=document.createTextNode("Hello Word")
- div.appendChild(textNode)
- console.log(div)
- console.log(textNode)
+// itemList.previousElementSibling.style.backgroundColor="yellow";
+//  let div=document.createElement('div');
+//  div.className="parentDiv";
+//  div.id="newDiv";
+//  div.setAttribute('title',"helloDiv");
+//  let textNode=document.createTextNode("Hello Word")
+//  div.appendChild(textNode)
+ 
+//  console.log(div)
+//  console.log(textNode)
+let form=document.getElementById('addForm');
+let itemList=document.getElementById('items');
+//console.log(form)
+form.addEventListener('submit',addItem);
+itemList.addEventListener('click',removeItem);
+function addItem(e){
+    //alert(e)
+    e.preventDefault();
+    let newItem=document.getElementById('item').value;
+    console.log(newItem)
+    let li=document.createElement('li');
+    li.className='list-group-item';
+    console.log(li);
+    //add text node with the input value
+   li.appendChild(document.createTextNode(newItem));
+   let deleteBtn=document.createElement('button');
+   deleteBtn.className='btn btn-danger btn-sm float-right delete';
+   deleteBtn.appendChild(document.createTextNode('X'));
+   li.appendChild(deleteBtn)
+
+    itemList.appendChild(li);
+}
+//Remove item
+function removeItem(e){
+console.log(1)
+if(e.target.classList.contains('delete')){
+    var li=e.target.parentElement;
+    itemList.removeChild(li);
+}
+
+}
+
+//add items
+// function addItem(e)
+// {
+//    // e.preventDefault();
+//     console.log(1)
+// }
